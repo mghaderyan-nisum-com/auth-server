7aa916d6442191fced333788773d298073435bf2
Meysam <yc04832@a4370562.federated.fds>
2016-06-22 10:00:35 -0700

    set cookie

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express')
 var app = express()
+var cookieParser = require('cookie-parser');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(cookieParser());
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
 app.get('/validateToken', function (req, res) {
+  res.cookie('sr_token', req.query.srtoken)
   res.json({
     result: true,
   });
