d5c83039d2372a79ef02e35f1ed2e53ed0a3e9fb
Meysam <yc04832@a4370562.federated.fds>
2016-06-22 13:02:21 -0700

    implemented validateToken

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 var cookieParser = require('cookie-parser');
+var request = require('request');
+var parseString = require('xml2js').parseString;
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -11,10 +15,34 @@ app.get('/', function(request, response) {
 })
 
 app.get('/validateToken', function (req, res) {
-  res.cookie('sr_token', req.query.srtoken)
-  res.json({
-    result: true,
-  });
+  var options = {
+    url: 'https://api-staging.shoprunner.com/validateToken',
+    qs: {
+      srtoken: req.query.srtoken
+    },
+    auth: {
+      user: 'bloom@shoprunner.com.srstg',
+      password: '2016Spring'
+    },
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+  request(options, (error, response, body) => {
+    if(error) {
+      return res.json({
+        result: false,
+      });
+     }
+     parser.parseString(body, (err, result) => {
+       console.log(body);
+       res.cookie('sr_token', req.query.srtoken);
+       return res.json({
+         result: result['sr:validateTokenResponse']['sr:validationResult'][0] === 'true',
+       });
+     });
+  })
 });
 
 app.listen(app.get('port'), function() {
